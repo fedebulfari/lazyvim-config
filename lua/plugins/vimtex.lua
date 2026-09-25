8cc57c54306795af60c35c6080544a0bdec49d0c
Federico Ernesto Bulfari <federicoernesto.bulfari@mail.polimi.it>
2026-09-25 13:15:12 +0200

    vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,6 +6,11 @@ return {
     init = function()
       vim.g.vimtex_view_general_viewer = "C:\\Users\\feder\\AppData\\Local\\SumatraPDF\\SumatraPDF.exe"
       vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          pcall(vim.fn.serverstart, "127.0.0.1:11989")
+        end,
+      })
     end,
   },
 }

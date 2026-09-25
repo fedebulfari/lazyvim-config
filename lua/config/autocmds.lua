b50b412a965431bc6e0ad63a0dd3f09c527e665e
Federico Ernesto Bulfari <federicoernesto.bulfari@mail.polimi.it>
2026-09-25 12:50:30 +0200

    fix

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- File: lua/config/autocmds.lua
+
+-- Intercetta l'apertura di qualsiasi file .pdf
+vim.api.nvim_create_autocmd("BufReadCmd", {
+  pattern = "*.pdf",
+  callback = function(opts)
+    -- Inserisci qui il percorso corretto di SumatraPDF
+    local sumatra_path = "C:\\Users\\feder\\AppData\\Local\\SumatraPDF\\SumatraPDF.exe"
+
+    -- Avvia Sumatra in modo indipendente (detach = true) passando il percorso del PDF
+    vim.fn.jobstart({ sumatra_path, opts.match }, { detach = true })
+
+    -- Chiude il buffer di testo illeggibile che Neovim aveva provato ad aprire
+    vim.api.nvim_buf_delete(opts.buf, { force = true })
+  end,
+})
--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -4,7 +4,7 @@ return {
     "lervag/vimtex",
     lazy = false,
     init = function()
-      vim.g.vimtex_view_general_viewer = "SumatraPDF"
+      vim.g.vimtex_view_general_viewer = "C:\\Users\\feder\\AppData\\Local\\SumatraPDF\\SumatraPDF.exe"
       vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
     end,
   },

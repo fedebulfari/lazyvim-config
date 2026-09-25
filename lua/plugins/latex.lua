de3989e58f07ae21fe9192e71c66c7a31ac49600
Federico Ernesto Bulfari <federicoernesto.bulfari@mail.polimi.it>
2026-09-25 10:53:28 +0200

    added latex

---
+++ lua/plugins/latex.lua
@@ -0,0 +1,11 @@
+-- File: lua/plugins/latex.lua
+return {
+  {
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+      vim.g.vimtex_view_general_viewer = "SumatraPDF"
+      vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+    end,
+  },
+}
